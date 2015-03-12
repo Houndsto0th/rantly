efc3cb15f74d8f55bbdcb52f7346ba9fe5aaa6de
Houndsto0th <bry.kinnear@gmail.com>
2015-03-12 17:59:12 -0600

    added icons to login form

--- config/environment.js
+++ config/environment.js
@@ -16,10 +16,10 @@ module.exports = function(environment) {
     contentSecurityPolicy: {
     'default-src': "'none'",
     'script-src': "'self'",
-    'font-src': "'self' http://fonts.gstatic.com",
+    'font-src': "'self' http://fonts.gstatic.com http://maxcdn.bootstrapcdn.com/",
     'connect-src': "'self' localhost:3000",
-    'img-src': "'self'",
-    'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com",
+    'img-src': "'self' http://www.gravatar.com/",
+    'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com http://maxcdn.bootstrapcdn.com/",
     'media-src': "'self'"
   },
 
