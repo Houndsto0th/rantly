d533bea3634c200f084aaa4a7c53bf71f54e8613
Houndsto0th <bry.kinnear@gmail.com>
2015-03-27 08:58:40 -0600

    added connection

--- config/environment.js
+++ config/environment.js
@@ -17,7 +17,7 @@ module.exports = function(environment) {
     'default-src': "'none'",
     'script-src': "'self'",
     'font-src': "'self' http://fonts.gstatic.com http://maxcdn.bootstrapcdn.com/",
-    'connect-src': "'self' localhost:3000",
+    'connect-src': "'self' localhost:3000 https://rantly-serverbk.herokuapp.com/",
     'img-src': "'self' http://www.gravatar.com/",
     'style-src': "'self' 'unsafe-inline' http://fonts.googleapis.com http://maxcdn.bootstrapcdn.com/",
     'media-src': "'self'"
